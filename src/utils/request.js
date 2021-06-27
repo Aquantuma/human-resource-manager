9e049df37dd81f53a3d914a4cc15775cd441cb22
Rozon <1160733063@qq.com>
2021-06-27 16:44:18 +0800

    响应拦截器的三种可能，网络请求失败的处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { Message } from 'element-ui'
+
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
@@ -10,7 +12,33 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use()
 
-// 相应拦截器
-service.interceptors.response.use()
+// 相应拦截器 相应有三种情况
+service.interceptors.response.use(
+  res => {
+    // 1. 请求成功
+    console.log('这是网络层面的成功')
+    console.log(res)
+    const { success } = res.data
+    if (success) {
+      // 1.1 数据成功 -> 返回数据
+      console.log('数据请求成功')
+      return res
+    } else {
+      // 1.2 数据失败 -> 提示错误+停止请求
+      console.log('数据请求失败')
+    }
+  },
+  err => {
+    // 2. 请求失败 -> 提示错误+停止请求
+    console.log('这是网络层面的失败')
+    console.dir(err)
+    // 弹出窗告诉用户
+    Message.error(err.message)
+    // 接着需要放行（报错）
+    // return 123 这种普通数据的相应会返回到.then
+    // 这里应该用Promise.reject 直接停止当前的请求过程
+    return Promise.reject(new Error(err.message))
+  }
+)
 
 export default service
