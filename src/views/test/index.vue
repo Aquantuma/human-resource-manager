677dca8b7f62fa8aaf003dc2da2ddf3a288c9b01
Rozon <1160733063@qq.com>
2021-06-27 10:30:42 +0800

    测试自定义校验函数的用法

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="test">
     <h1>登录</h1>
-    <el-form :model="loginForm" :rules="loginRules" label-width="100px">
+    <el-form :model="loginForm" :rules="loginRules">
       <el-form-item label="用户名" prop="username">
         <el-input v-model="loginForm.username" type="text" />
       </el-form-item>
       <el-form-item label="密码" prop="password">
         <el-input v-model="loginForm.password" type="password" />
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPassword">
+        <el-input v-model="loginForm.checkPassword" type="password" />
       </el-form-item>
     </el-form>
   </div>
@@ -15,10 +18,20 @@
 <script>
 export default {
   data() {
+    const validCheckPassword = (rule, value, callback) => {
+      // console.log(rule, value, callback)
+      const isValid = value === this.loginForm.password
+      if (isValid) {
+        callback()
+      } else {
+        callback(new Error('两次输入的密码必须一致'))
+      }
+    }
     return {
       loginForm: {
         username: '',
-        password: ''
+        password: '',
+        checkPassword: ''
       },
       loginRules: {
         username: [
@@ -26,6 +39,10 @@ export default {
         ],
         password: [
           { required: true, message: '密码不能为空', trigger: 'blur' }
+        ],
+        checkPassword: [
+          { required: true, message: '校验密码不能为空', trigger: 'blur' },
+          { validator: validCheckPassword, trigger: 'blur' }
         ]
       }
     }
