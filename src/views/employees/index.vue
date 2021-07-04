8162ec708f93c97b9b7e799f6ff892c3f5d04965
Rozon <1160733063@qq.com>
2021-07-04 10:49:20 +0800

    封装并使用公共工具栏组件

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <PageTools :show-before="true">
+        <template #before>一共166条数据</template>
+        <template #after>
+          <el-button type="warning" size="small">导入</el-button>
+          <el-button type="danger" size="small">导出</el-button>
+          <el-button type="primary" size="small">新增员工</el-button>
+        </template>
+      </PageTools>
     </div>
   </div>
 </template>
 
 <script>
+import PageTools from '@/components/PageTools'
 export default {
-
+  components: {
+    PageTools
+  }
 }
 </script>
 
