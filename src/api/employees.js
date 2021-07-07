ef0071e2fb47d5a7464cc94622017546f5ba8033
Rozon <1160733063@qq.com>
2021-07-08 03:05:53 +0800

    将导入的数据的中文键名转换为英文，同时修改excel导入的日期格式

--- src/api/employees.js
+++ src/api/employees.js
@@ -8,3 +8,12 @@ export const getEmployeesList = (params) => {
     params
   })
 }
+
+// 批量导入员工
+export const importEmployees = (data) => {
+  return request({
+    url: '/sys/user/batch',
+    method: 'post',
+    data
+  })
+}
--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,13 +1,49 @@
 <template>
-  <UploadExcel />
+  <UploadExcel :on-success="uploadExcel" />
 </template>
 
 <script>
 export default {
-
+  methods: {
+    uploadExcel(data) {
+      // 表头信息数组 data.header
+      // 表格行信息数组 data.results
+      //   console.log(data.header, data.results)
+      // 定义中英文键名转换的字典
+      const dict = {
+        入职日期: 'timeOfEntry',
+        手机号: 'mobile',
+        姓名: 'username',
+        转正日期: 'correctionTime',
+        工号: 'workNumber'
+      }
+      const newData = data.results.map((item) => {
+        const newEmployee = {}
+        Object.keys(item).forEach((key) => {
+          // Excel导出的时间值与时间戳不同，需要转换
+          if (key === '入职日期' || key === '转正日期') {
+            item[key] = new Date(this.correctExcelTime(item[key], '/'))
+          }
+          newEmployee[dict[key]] = item[key]
+        })
+        return newEmployee
+      })
+      console.log(newData)
+    },
+    correctExcelTime(num, char) {
+      const time = new Date((num - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (char && char.length === 1) {
+        return year + char + month + char + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
