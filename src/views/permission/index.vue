d2a4e296a29054bac3cd66a24632990610c1f17f
Rozon <1160733063@qq.com>
2021-07-10 22:56:18 +0800

    权限控制列表的创建与数据渲染

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限
-      </h2>
+      <!-- 工具栏 -->
+      <PageTools>
+        <template #after>
+          <el-button type="primary" size="small">
+            <i class="el-icon-plus" />添加权限
+          </el-button>
+        </template>
+      </PageTools>
+      <!-- 页面渲染表格 -->
+      <el-table :data="permissionList" row-key="id" :default-expand-all="true">
+        <el-table-column label="权限名称" prop="name" />
+        <el-table-column label="权限标识" prop="code" />
+        <el-table-column label="权限描述" prop="description" />
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button type="text" :disabled="scope.row.type===2">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
+import { listToTreeData } from '@/utils/index'
 export default {
-
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const res = await getPermissionList()
+      console.log(res)
+      this.permissionList = listToTreeData(res, '0')
+    }
+  }
 }
 </script>
 
