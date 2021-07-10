7445bf2e0f0960af22206c5c6a900b1b5374a9b1
Rozon <1160733063@qq.com>
2021-07-10 19:18:25 +0800

    原生打印和调用vue-print-nb插件打印员工信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -26,7 +26,7 @@ service.interceptors.request.use(
     const token = store.getters.token
     if (token) {
       if (checkTimeOut()) {
-        console.log('token已失效')
+        // console.log('token已失效')
         // 调用actions中的退出登录方法
         store.dispatch('user/logout')
         // 跳转到登录页
@@ -49,18 +49,18 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   res => {
     // 1. 请求成功
-    console.log('这是网络层面的成功')
+    // console.log('这是网络层面的成功')
     // console.log(res)
     const { success, message, data } = res.data
     if (success) {
       // 1.1 数据成功 -> 返回数据
-      console.log('数据请求成功')
+      // console.log('数据请求成功')
       // 这里不直接返回res
       // 而是从res.data中解构处data （res.data.data）
       return data
     } else {
       // 1.2 数据失败 -> 提示错误+停止请求
-      console.log('数据请求失败')
+      // console.log('数据请求失败')
       // 弹窗提示错误
       Message.error(message)
       // 拒绝掉当前的请求
